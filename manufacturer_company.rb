ac4875f052aee433a8ccaabcdbe01fb27283a02a
yurlov43 <yurlov43@gmail.com>
2021-11-04 17:47:10 +0300

    code duplication was fixed in the manufacturer_company module

--- manufacturer_company.rb
+++ manufacturer_company.rb
@@ -1,11 +1,3 @@
 module ManufacturerCompany
   attr_accessor :manufacturer_company
-
-  def set_manufacturer_company(company)
-    self.manufacturer_company = company
-  end
-
-  def get_manufacturer_company
-    self.manufacturer_company
-  end
 end
